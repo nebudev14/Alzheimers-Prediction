5dde3fdd1a0e5903b6b25c1f9e17a12d35c37cb8
warren yun <xxwarrenhdxx@gmail.com>
2021-11-24 13:19:29 -0500

    Switch to python bc it's better

---
+++ main.py
@@ -0,0 +1,50 @@
+from sklearn.metrics import confusion_matrix
+import numpy as np
+from scipy.io import loadmat
+import os
+from pywt import wavedec
+from functools import reduce
+from scipy import signal
+from scipy.stats import entropy
+from scipy.fft import fft, ifft
+import pandas as pd
+from sklearn.model_selection import train_test_split, StratifiedKFold
+from sklearn.model_selection import RandomizedSearchCV
+from sklearn.preprocessing import StandardScaler
+import matplotlib.pyplot as plt
+import scipy
+from sklearn import metrics
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import KFold,cross_validate
+from sklearn.metrics import classification_report
+import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
+from sklearn.model_selection import cross_val_score
+import seaborn as sns
+
+data_cross = pd.read_csv('./data/oasis_cross-sectional.csv')
+data_long = pd.read_csv('./data/oasis_longitudinal.csv')
+
+print(data_cross.info())
+print("-----")
+print(data_long.info())
+
+print("NA vals:")
+print(data_cross.isna().sum())
+print("----------")
+print(data_long.isna().sum())
+
+# drop all rows with na/undefined values
+data_cross.dropna(subset=['CDR'], inplace=True)
+
+# remove unneccessary columns in both subsets
+data_cross.drop(columns=['ID', 'Delay'], inplace=True)
+data_long = data_long.rename(columns={'EDUC': 'Educ'})
+data_long.drop(columns=['Subject ID', 'MRI ID', 'Group', 'Visit', 'MR Delay'], inplace=True)
+
+print("----------------new data")
+data = pd.concat([data_cross, data_long])
+print(data.head())
